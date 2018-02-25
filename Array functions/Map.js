d86447bd75d98d9196dcf9bbb22967a1ae134666
Colton Travers <contactcolt@gmail.com>
2018-02-25 05:43:29 -0600

    Array Map Addition

---
+++ Array functions/Map.js
@@ -0,0 +1,23 @@
+// Array map is a higher order function that returns a new array that has a function applied to each element in the array.
+
+let Arr = [1,2,3,4,5];
+
+// Let's map through the array and simply add one to each item.
+
+let newArr = Arr.map(function(item,index) {
+    return item+1;
+});
+
+// Remember it returns a new array - it doesn't modify the original.
+// You could simply set the new array = to the original array variable to avoid any bugs.
+// EX:
+
+let Arr = [1,2,3,4,5];
+Arr = Arr.map(function(item,index) {
+    return item+1;
+});
+
+// We can shorten this using ES6.
+
+let Arr = [1,2,3,4,5];
+Arr = Arr.map((item,index) => item+1);

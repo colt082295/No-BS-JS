31e6bc38e9c04a5b4df04c990e80929124b26b24
Colton Travers <contactcolt@gmail.com>
2018-02-26 01:38:15 -0600

    Changed wording.

--- For loops/forInLoop.js
+++ For loops/forInLoop.js
@@ -1,4 +1,4 @@
-// A forIn loop is pretty similar to the classic for loop. Some differences are that it takes less setup code and it less efficient(because it has to find next property in the object that is enumerable). Typically you wouldn't use this to iterate through an array.
+// A forIn loop is pretty similar to the classic for loop. Some differences are that it takes less setup code and it less efficient(because it has to find next property in the object that is enumerable). Typically you wouldn't use this to iterate through an array, but instead through an object.
 
 let arr = [1,5,6,7,3,8];
 
@@ -6,7 +6,7 @@ for(num in arr) { // For each number in the array
     console.log(num);
 }
 
-// A key feature of the forIn loop is the ability to iterate through objects.
+// Now we'll go through an object.
 
 var obj = {
     a: "Val 1",
